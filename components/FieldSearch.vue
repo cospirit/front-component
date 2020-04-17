434d18d2e7728fd986d083a0b53a1eafb4e9978b
Florent Lavy <florent.lavy@cospirit.com>
2020-04-17 17:02:35 +0200

    Can force new value in search inputs

--- components/FieldSearch.vue
+++ components/FieldSearch.vue
@@ -8,14 +8,12 @@
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
-import { Prop, Watch } from "vue-property-decorator";
+import { Prop } from "vue-property-decorator";
 import _ from "lodash";
 import EventBus from "../EventBus";
 
 @Component({})
 export default class FieldSearch extends Vue {
-    protected filterInput: string | null = null;
-
     @Prop() public filterName!: string;
     @Prop() public label!: string;
     @Prop() public placeholder!: string;
@@ -24,15 +22,10 @@ export default class FieldSearch extends Vue {
     @Prop({default: "validate"}) public classes!: string;
 
     public get value(): string {
-        if (null === this.filterInput) {
-            this.filterInput = this.defaultValue;
-        }
-
-        return this.filterInput;
+        return this.defaultValue;
     }
 
     public set value(newValue) {
-        this.filterInput = newValue;
         EventBus.$emit("update-filter", _.set({}, this.filterName, newValue));
     }
 }

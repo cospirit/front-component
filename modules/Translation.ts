fb7e4a71840d01d0e84013b011f5ac51408ee716
Jvigneron <jonathan.vigneron@cospirit.com>
2023-05-31 17:17:41 +0200

    feat: add languages in translation

--- modules/Translation.ts
+++ modules/Translation.ts
@@ -7,19 +7,39 @@ interface Translation {
     source: string;
     target: string;
     context: string;
+    language: Language;
+}
+
+interface Language {
+    code: string;
+    language: string;
 }
 
 export default {
     state: {
         translations: [],
+        languages: [],
     },
     getters: {
+        getAllLanguages: (state: any): any[] => {
+            return state.languages;
+        },
         translate: (state: any) => {
+            const userLanguage = localStorage.getItem('userLanguage');
+            let languageCode = navigator.language.substr(0, 2);
+            if (userLanguage) {
+                languageCode = userLanguage;
+            }
+            if (!state.languages.find((language: Language) => {
+                return language.code === languageCode;
+            })) {
+                languageCode = 'en';
+            }
 
             return (translate: string) => {
                 let result = translate;
                 state.translations.forEach((translation: Translation) => {
-                    if (translate === translation.source) {
+                    if (translate === translation.source && translation.language.code === languageCode) {
                         result = translation.target;
                     }
                 });
@@ -39,6 +59,7 @@ export default {
                         "target",
                         "context",
                         "language.code",
+                        "language.language",
                     ],
                 };
 
@@ -47,6 +68,13 @@ export default {
                     params,
                     (data: Data) => {
                         state.translations = data.data;
+                        data.data.forEach((line: any) => {
+                            if (!state.languages.find((language: Language) => {
+                                return language.code === line.language.code;
+                            })) {
+                                state.languages.push(line.language);
+                            }
+                        });
                     },
                     (error: Data) => {
                         state.translations = null;
